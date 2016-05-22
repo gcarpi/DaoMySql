8c86d6df79960ad6ac5d8b40438acd89501d6b6b
Rafael Souza <rafinhatecla.ds@gmail.com>
2016-05-22 17:26:33 -0300

    correção

--- Persistencia/Persistencia/DAO/CategoriaDAO.cs
+++ Persistencia/Persistencia/DAO/CategoriaDAO.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Persistencia.Interface;
 using Persistencia.Modelo;
 using Persistencia.Util;
 using System;
@@ -10,9 +11,9 @@ using System.Threading.Tasks;
 
 namespace Persistencia.DAO
 {
-    public class CategoriaDAO : Interface.IDAO<Categoria>, IDisposable
+    public class CategoriaDAO : IDAO<Categoria>, IDisposable
     {
-        private Interface.IConnection _connection;
+        private Connection _connection;
 
         public CategoriaDAO()
         {
